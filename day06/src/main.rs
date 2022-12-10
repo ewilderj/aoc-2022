d4de2f700bcabd5d57be6feaf941fa5292d0e1fe
Edd Wilder-James <edd@me.com>
2022-12-10 12:51:27 -0800

    day6 solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+
+fn detect(s: &str, n: usize) -> usize {
+    for i in 0..(s.len() - n) {
+        let h: HashSet<char> = s[i..i + n].chars().collect();
+        if h.len() == n {
+            return i + n;
+        }
+    }
+    unreachable!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = include_str!("../input.txt").trim();
+    println!("part1: {}", detect(&s, 4));
+    println!("part2: {}", detect(&s, 14));
 }
