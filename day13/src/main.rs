c5598713c20e56e9256c98387224383592441ccf
Edd Wilder-James <edd@me.com>
2022-12-25 16:58:33 -0800

    Terser

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -23,25 +23,12 @@ impl Signal {
     // this is the logic for part1:
     // 0 for equal, 1 for right order, -1 for wrong order
     fn compare(l: &Signal, r: &Signal) -> i32 {
-        if let Signal::I(m) = l {
-            if let Signal::I(n) = r {
-                // both numbers:
-                return (n - m).signum();
-            } else {
-                // r is a list, so promote l and try again
-                let pl = Signal::L(vec![l.clone()]);
-                return Self::compare(&pl, r);
-            }
-        } else {
-            // l is a list, so promote r and try again
-            if let Signal::I(_) = r {
-                let pr = Signal::L(vec![r.clone()]);
-                return Self::compare(l, &pr);
-            }
-
-            // both sides are lists, so compare items
-            let mut i = 0;
-            if let (Signal::L(a), Signal::L(b)) = (l, r) {
+        match (l, r) {
+            (Signal::I(a), Signal::I(b)) => return (b - a).signum(),
+            (Signal::I(_), Signal::L(_)) => return Self::compare(&l.promote(), r),
+            (Signal::L(_), Signal::I(_)) => return Self::compare(l, &r.promote()),
+            (Signal::L(a), Signal::L(b)) => {
+                let mut i = 0;
                 let lr = (b.len() as i32 - a.len() as i32).signum();
                 loop {
                     if i >= a.len() || i >= b.len() {
@@ -55,10 +42,16 @@ impl Signal {
                 }
             }
         }
-        return 0;
+    }
+
+    fn promote(&self) -> Signal {
+        Signal::L(vec![self.clone()])
+    }
+
+    fn marker(n: i32) -> Signal {
+        Signal::I(n).promote().promote()
     }
 }
-
 
 impl Ord for Signal {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -167,16 +160,10 @@ fn parse_aux(ts: &[Token]) -> Result<(usize, Signal), &str> {
 }
 
 fn parse(t: &TokenList) -> Result<Signal, &str> {
-    let r = parse_aux(&t[1..t.len() - 1]);
-    if r.is_ok() {
-        let (_, v) = r.unwrap();
-        return Ok(v);
+    match parse_aux(&t[1..t.len() - 1]) {
+        Ok((_, v)) => Ok(v),
+        Err(e) => Err(e),
     }
-    return Err(r.err().unwrap());
-}
-
-fn marker(n: i32) -> Signal {
-    Signal::L(vec![Signal::L(vec![Signal::I(n)])])
 }
 
 fn main() {
@@ -201,11 +188,11 @@ fn main() {
         .map(|x| parse(&x).unwrap())
         .collect();
 
-    l.push(marker(2));
-    l.push(marker(6));
+    l.push(Signal::marker(2));
+    l.push(Signal::marker(6));
     l.sort();
 
-    let (m2, m6) = (marker(2), marker(6));
+    let (m2, m6) = (Signal::marker(2), Signal::marker(6));
 
     let a = l.iter().position(|m| *m == m2).unwrap() + 1;
     let b = l.iter().position(|m| *m == m6).unwrap() + 1;
